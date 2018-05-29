64a2c850485f7d0117b6db1da55651e9be465a3f
lihui <1332508387@qq.com>
2018-05-29 11:28:19 +0800

    添加和修改

---
+++ src/main/java/com/lh/mode/proxy/StaticProxyTest.java
@@ -0,0 +1,33 @@
+package com.lh.mode.proxy;
+
+public class StaticProxyTest {
+    interface Work {
+        void doWork();
+    }
+
+    static class Student implements Work {
+        @Override
+        public void doWork() {
+            System.out.println("学生自己写作业。。。");
+        }
+    }
+
+    static class StudentProxy implements Work {
+        private Work work;
+
+        public StudentProxy(Student student) {
+            this.work = student;
+        }
+
+        @Override
+        public void doWork() {
+            System.out.println("我代理学生写作业。。。");
+            work.doWork();
+        }
+    }
+
+    public static void main(String[] args) {
+        StudentProxy proxy = new StudentProxy(new Student());
+        proxy.doWork();
+    }
+}
