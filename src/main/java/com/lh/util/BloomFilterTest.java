64f3c0d8b1a165fc01692eb12feac6e9f3f0eb3e
lihui <1332508387@qq.com>
2018-06-01 14:01:15 +0800

    添加和修改

--- src/main/java/com/lh/util/BloomFilterTest.java
+++ src/main/java/com/lh/util/BloomFilterTest.java
@@ -7,7 +7,7 @@ import java.util.BitSet;
 
 public class BloomFilterTest {
     public static void main(String[] args) {
-        BloomFilter<String> bloomFilter = new BloomFilter<String>(0.1, 100);
+        BloomFilter<String> bloomFilter = new BloomFilter<String>(0.01, 1000);
         bloomFilter.add("拉普拉斯");
         System.out.println(bloomFilter.contains("拉普拉斯"));
         System.out.println(bloomFilter.contains("拉普斯"));
@@ -15,11 +15,20 @@ public class BloomFilterTest {
 
     @Test
     public void testBigSet() {
-        BitSet set = new BitSet();
-        set.set(1);
-        set.set(2);
-        int cardinality = set.cardinality();
-        System.out.println(cardinality);
-        System.out.println(set.get(1));
+//        BitSet set = new BitSet();
+//        set.set(1);
+//        set.set(2);
+//        int cardinality = set.cardinality();
+//        System.out.println(cardinality);
+//        System.out.println(set.get(1));
+        byte b = 127;
+        char c = '龘';
+        String str = "";
+        byte[] bytes = str.getBytes();
+        for (int i = 0; i < str.length(); i++) {
+            System.out.println(bytes[i]);
+        }
+
+        System.out.println((byte)c);
     }
 }
