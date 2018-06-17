e0c77fb52c08c93b1c7df7a162da302f955a413b
1332508387 <1332508387@qq.com>
2018-06-17 16:26:17 +0800

    实现栈

--- src/main/java/com/lh/data_structure/stack/MyStack.java
+++ src/main/java/com/lh/data_structure/stack/MyStack.java
@@ -1,4 +1,40 @@
 package com.lh.data_structure.stack;
 
 public class MyStack {
+    private static final int CAPACITY = 1 << 4;
+    private int top = -1;
+    private Object[] stack = new Object[CAPACITY];
+
+    public Object pop() {
+        if (top == -1) {
+            return null;
+        }
+        return stack[top--];
+    }
+
+    public boolean push(Object ele) {
+        if (top == CAPACITY - 1) {
+            return false;
+        }
+        stack[++top] = ele;
+        return true;
+    }
+
+    public boolean isEmpty() {
+        return top == -1;
+    }
+
+    public static void main(String[] args) {
+        MyStack myStack = new MyStack();
+        System.out.println(myStack.isEmpty());
+        myStack.push("aaa");
+        myStack.push("bbb");
+        myStack.push("ccc");
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.pop());
+        System.out.println(myStack.isEmpty());
+    }
+
 }
